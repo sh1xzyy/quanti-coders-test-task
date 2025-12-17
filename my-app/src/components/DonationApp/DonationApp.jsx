d87be4a16038314ad08159720daa0aea120b4027
sh1xzyy <alekslungol@gmail.com>
2025-12-17 21:59:53 +0300

    feat(form): add DonationApp

---
+++ my-app/src/components/DonationApp/DonationApp.jsx
@@ -0,0 +1,11 @@
+import { HelpTypesSection } from "../HelpTypesSection/HelpTypesSection";
+import { RegistrationFormSection } from "../RegistrationFormSection/RegistrationFormSection";
+
+export const DonationApp = () => {
+  return (
+    <div>
+      <HelpTypesSection />
+      <RegistrationFormSection />
+    </div>
+  );
+};
