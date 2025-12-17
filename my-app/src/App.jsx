74c8c211c2223197cc58ad0203abe4caa32223c4
sh1xzyy <alekslungol@gmail.com>
2025-12-17 21:48:54 +0300

    feat(form): add PaymentMethod

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,9 +1,11 @@
 import { HelpTypeCard } from "./components/HelpTypeCard/HelpTypeCard";
+import { PaymentMethod } from "./components/PaymentMethod/PaymentMethod";
 
 function App() {
   return (
     <div>
       <HelpTypeCard />
+      <PaymentMethod />
     </div>
   );
 }
